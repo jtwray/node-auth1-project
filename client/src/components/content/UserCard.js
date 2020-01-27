f94ee57962e8c958c1c8c9436bd2b076a2588b96
Brianna Keune <briannakeune@gmail.com>
2020-01-27 17:47:13 -0600

    added a user card for users to display

---
+++ client/src/components/content/UserCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+export default function UserCard(props) {
+  console.log("UserCard: Props: ", props);
+
+  return (
+    <div>
+      <Card
+        header={props.user.username}
+        meta="User"
+        description={`Contact me @ ${props.user.email}`}
+      />
+    </div>
+  );
+}
