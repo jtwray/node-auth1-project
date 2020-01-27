d028ea011645dc0b7059c6e12b104676c81865be
Brianna Keune <briannakeune@gmail.com>
2020-01-27 11:23:09 -0600

    knexfile setup

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/auth_example.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: ".data/seeds"
+    }
+  }
+};
