7cbe97c900426cc6b062ed6eab3472a4463bcf15
Brianna Keune <briannakeune@gmail.com>
2020-01-27 12:15:37 -0600

    users model

---
+++ api/auth/model.js
@@ -0,0 +1,67 @@
+const db = require("../dbConfig.js");
+
+module.exports = {
+  getUser,
+  getUserByProperty,
+  addUser,
+  updateUser,
+  removeUser
+};
+
+/**
+ * Gets a list of usernames
+ * @returns {Array} -- of all current usernames.
+ */
+function getUser() {
+  return db("users").select("users.username");
+}
+
+/**
+ * Gets a user by a property of the users table.
+ * @param {object} property -- could be id, username, email.
+ * @returns {user} -- id, username, and email of the user you searched for.
+ */
+function getUserByProperty(property) {
+  return db("users")
+    .select("users.id", "users.username", "users.email")
+    .where(property)
+    .first();
+}
+
+/**
+ * Adds a new user to the database
+ * @param {object} newUser -- needs username, password, and email
+ * @returns {user} returns the new user created with id, username, and email.
+ */
+async function addUser(newUser) {
+  const [id] = await db("users").insert(newUser, "id");
+  const user = await getUserByProperty(id);
+  return user;
+}
+
+/**
+ * Updates a user in database.
+ * @param {integer} id -- existing id in database
+ * @param {object} changes -- whatever changes you'd like to update, username, email, or password.
+ * @returns {updatedUser} -- returns user with id, username, and email with updated information.
+ */
+async function updateUser(id, changes) {
+  await db("users")
+    .where({ id })
+    .update(changes);
+  const updatedUser = await getUserByProperty(id);
+  return updatedUser;
+}
+
+/**
+ * Deletes a user in database
+ * @param {integer} id -- integer of existing user in database.
+ * @returns {Array} users so FE can easily update current list of users if in use on delete.
+ */
+async function removeUser(id) {
+  await db("users")
+    .where({ id })
+    .del();
+  const users = await getUser();
+  return users;
+}
